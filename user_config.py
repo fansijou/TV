1167f29c848535591170faffedfc54967352b4f4
Fansi <fansijou@outlook.com>
2024-03-28 16:22:13 +0900

    Create user_config.py

---
+++ user_config.py
@@ -0,0 +1,19 @@
+source_file = "user_demo.txt"
+final_file = "user_result.txt"
+favorite_list = [
+    "CCTV-13",
+    "CCTV-5",
+    "CCTV-5+",
+    "CCTV-13",
+    "TVBS新闻",
+    "凤凰资讯",
+    "翡翠台",
+    "东方卫视",
+]
+favorite_page_num = 6
+default_page_num = 4
+urls_limit = 15
+response_time_weight = 0.5
+resolution_weight = 0.5
+recent_days = 30
+ipv_type = "ipv6"
